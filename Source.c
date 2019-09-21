3386de0c2133abaf8a3b0c5c9919306b8e1a1cb8
Ree <ree@pop-os.localdomain>
2019-09-20 20:59:21 -0700

    Make a new flag for the max value

--- Source.c
+++ Source.c
@@ -3,22 +3,21 @@ float min_in = 0.0;
 float max_in = 0.0;
 float avg_num = 0.0;
 float sum = 0.0;
-float user_input = 1.0;
+float user_input = 0.0;
 int num_counter = 0;
 int dummy = 0;
 
 int main()
 {
-	printf("please enter number. When done enter "0" \n" );
+	printf("please enter number. When done enter 0 \n" );
 	scanf("%f", &user_input);
-	_Bool min = 1;
+	_Bool min_flag = 1; // min flag
 	while(user_input !=0)
-
 		{
 		sum += user_input;
-		if (min) {
-			min_in = user_input;
-			min = 0;
+		if (min_flag) {
+			min_in = user_input; // Had trouble with min always being 0.
+			min_flag = 0;   // Use a flag in the first loop to set the min value
 		} else if(user_input < min_in) {
 			min_in = user_input;
 		}else if(user_input>=max_in)
